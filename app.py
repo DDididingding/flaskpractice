09dfa364257e1759fc747120b0554facf8a4942c
junhyun <erik1201@naver.com>
2023-06-27 21:48:45 +0900

    feat: Implement database access and list viewing using Pandas
    Utilize the Pandas package to establish a connection to the database and retrieve data for viewing. Implemented the functionality to display a list of items retrieved from the database.
    
    Please note that this commit includes changes to the codebase to incorporate Pandas for database access and list rendering.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sys, database
 
 app = Flask(__name__)
 
@@ -12,20 +13,28 @@ def apply():
 
 @app.route('/list')
 def list():
-    return render_template('list.html')
+    house_list = database.load_list()
+    length = len(house_list)
+
+    return render_template('list.html', house_list = house_list, length = length)
 
 @app.route('/applyphoto')
 def applyphoto():
     location = request.args.get("location")
     cleaness = request.args.get("clean")
     builtin = request.args.get("built")
-    print(location, cleaness, builtin)
+    if cleaness == None:
+        cleaness = False
+    else:
+        cleaness = True
+
+    database.save(location, cleaness, builtin)
     return render_template('apply_photo.html')
 
 @app.route('/upload_done', methods=["POST"])
 def upload_done():
     uploaded_files = request.files["file"]
-    uploaded_files.save("static/img/{}.jpeg".format(1))
+    uploaded_files.save("static/img/{}.jpeg".format(database.now_index()))
 
     return redirect(url_for("index"))
 
